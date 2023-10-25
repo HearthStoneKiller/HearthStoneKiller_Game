184c4a60bdc7c876f174df523f951a07da64ba6b
Maslakov Artem <145763455+HearthStoneKiller@users.noreply.github.com>
2023-10-25 22:32:13 +0300

    Added viewer managers

---
+++ Assets/Scripts/GameViewer/GameViewerManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]public class GameViewerManager : MonoBehaviour
+{
+    [SerializeField]private Player firstPlayer;
+    [SerializeField]private PlayerScreenViewer firstPlayerScreenViewer;
+    [SerializeField]private Player secondPlayer;
+    [SerializeField]private PlayerScreenViewer secondPlayerScreenViewer;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
