db31cc35bab523d29248a2b5816a7154703780bb
Maslakov Artem <145763455+HearthStoneKiller@users.noreply.github.com>
2023-10-25 13:03:56 +0300

    Created Card prefabs

---
+++ Assets/Scripts/GameViewer/PlayerScreenViewer.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]public class PlayerScreenViewer : MonoBehaviour
+{
+    [SerializeField]private Player currentPlayer;
+    [SerializeField]private Player enemyPlayer;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
